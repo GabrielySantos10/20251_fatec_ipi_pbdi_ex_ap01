4a7e69fbf63e9f87f5c510a22a314b5070cda0a7
Gabriely Santos <gabriely.s.santos085@gmail.com>
2025-03-16 22:03:55 -0300

    test: testa a multiplicacao

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -5,3 +5,5 @@ b = 3
 print(f'{a} + {b} = {calculadora.somar(a,b)}')
 # 2 - 3  = -1
 print(f'{a} - {b} = {calculadora.subtrair(a,b)}')
+# 2 * 3  = 6
+print(f'{a} * {b} = {calculadora.multiplicar(a,b)}')
