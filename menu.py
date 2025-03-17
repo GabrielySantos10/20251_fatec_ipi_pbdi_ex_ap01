9fa1be696230bae9c015991030ace2f73d58f2d2
Gabriely Santos <gabriely.s.santos085@gmail.com>
2025-03-16 22:34:21 -0300

    feat: implementa o menu com as operacoes

---
+++ menu.py
@@ -0,0 +1,27 @@
+import calculadora
+def menu():
+     while True:
+         print("Escolha uma operação:\n1. Somar\n2. Subtrair\n3. Multiplicar\n4. Dividir\n0. Sair")
+         opcao = int(input("Digite sua opção: "))
+         
+         
+         if opcao == '0':
+             print("\nSaindo...")
+             break
+         
+         if opcao in ['1', '2', '3', '4']:
+             a = float(input("\nDigite o primeiro número: "))
+             b = float(input("Digite o segundo número: "))
+             
+             if opcao == '1':
+                 print(f"\nResultado: {a} + {b} = {calculadora.somar(a, b)}\n")
+             elif opcao == '2':
+                 print(f"\nResultado: {a} - {b} = {calculadora.subtrair(a, b)}\n")
+             elif opcao == '3':
+                 print(f"\nResultado: {a} * {b} = {calculadora.multiplicar(a, b)}\n")
+             elif opcao == '4':
+                 print(f"\nResultado: {a} / {b} = {calculadora.dividir(a, b)}\n")
+         else:
+             print("\nOpção inválida! Tente novamente.")
+ 
+menu()
